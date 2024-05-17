4b61e6dc8ffda00a0545f49160371ac08db1c013
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-05-17 12:18:36 +0530

    change images

--- src/Components/Teachers_Gallary/Teachersdata.js
+++ src/Components/Teachers_Gallary/Teachersdata.js
@@ -1,384 +1,383 @@
 let teacherdata = [
     {
-        "image": "https://picsum.photos/200",
-        "name": "John Doe",
+        "image": "https://www.leaderbiography.com/wp-content/uploads/2023/09/qrafqeq.png",
+        "name": "Amitabh Bachchan",
         "subject": "Mathematics",
         "experience": 10,
     },
     {
-        "image": "https://picsum.photos/201",
-        "name": "Jane Smith",
+        "image": "https://staticimg.amarujala.com/assets/images/2017/06/07/shah-rukh-khan_1496836513.jpeg",
+        "name": "Shah Rukh Khan",
         "subject": "English",
         "experience": 8,
     },
     {
-        "image": "https://picsum.photos/202",
-        "name": "Michael Johnson",
+        "image": "https://www.koimoi.com/wp-content/new-galleries/2023/09/aish.jpg",
+        "name": "Aishwarya Rai Bachchan",
         "subject": "Science",
         "experience": 12
     },
     {
-        "image": "https://picsum.photos/203",
-        "name": "Emily Davis",
+        "image": "https://www.livemint.com/lm-img/img/2024/02/14/1600x900/Deepika_Padukone_BAFTA_2024_Presenter_1707897095641_1707897095805.jpg",
+        "name": "Deepika Padukone",
         "subject": "History",
         "experience": 6
     },
     {
-        "image": "https://picsum.photos/204",
-        "name": "David Wilson",
+        "image": "https://variety.com/wp-content/uploads/2023/11/Salman-Khan-Tiger-3.jpg",
+        "name": "Salman Khan",
         "subject": "Geography",
         "experience": 9
     },
     {
-        "image": "https://picsum.photos/205",
-        "name": "Sarah Thompson",
+        "image": "https://assets.teenvogue.com/photos/5ae27aee9763b71947f27141/16:9/w_2560%2Cc_limit/GettyImages-951562696.jpg",
+        "name": "Priyanka Chopra",
         "subject": "Art",
         "experience": 7
     },
     {
-        "image": "https://picsum.photos/206",
-        "name": "Robert Anderson",
+        "image": "https://static.tnn.in/thumb/msid-109112487,thumbsize-34802,width-1280,height-720,resizemode-75/109112487.jpg",
+        "name": "Akshay Kumar",
         "subject": "Music",
         "experience": 5
     },
     {
-        "image": "https://picsum.photos/207",
-        "name": "Jessica Martinez",
+        "image": "https://images.firstpost.com/uploads/2024/01/Kareena.jpg",
+        "name": "Kareena Kapoor Khan",
         "subject": "Physical Education",
         "experience": 11
     },
     {
-        "image": "https://picsum.photos/208",
-        "name": "Christopher Taylor",
+        "image": "https://exchange4media.gumlet.io/news-photo/97642-BrandRanveer.jpg",
+        "name": "Ranveer Singh",
         "subject": "Computer Science",
         "experience": 4
     },
     {
-        "image": "https://picsum.photos/209",
-        "name": "Amanda Thomas",
+        "image": "https://www.livemint.com/lm-img/img/2023/04/30/600x338/INDIA-ENTERTAINMENT-CINEMA-BOLLYWOOD-0_1682873302752_1682873365625.jpg",
+        "name": "Alia Bhatt",
         "subject": "Foreign Language",
         "experience": 10
     },
     {
-        "image": "https://picsum.photos/210",
-        "name": "Daniel White",
+        "image": "https://images.hindustantimes.com/img/2021/02/26/1600x900/WhatsApp_Image_2021-02-26_at_8.11.51_PM_1614355980386_1614355991981.jpeg",
+        "name": "Ajay Devgn",
         "subject": "Social Studies",
         "experience": 8
     },
     {
-        "image": "https://picsum.photos/211",
-        "name": "Melissa Harris",
+        "image": "https://static.tnn.in/thumb/msid-109345703,thumbsize-24222,width-1280,height-720,resizemode-75/109345703.jpg",
+        "name": "Anushka Sharma",
         "subject": "Biology",
         "experience": 12
     },
     {
-        "image": "https://picsum.photos/212",
-        "name": "Matthew Garcia",
+        "image": "https://img.mensxp.com/media/content/2021/Jan/10_5ffade95011e7.jpeg?w=900&h=618&cc=1",
+        "name": "Hrithik Roshan",
         "subject": "Chemistry",
         "experience": 6
     },
     {
-        "image": "https://picsum.photos/213",
-        "name": "Laura Robinson",
+        "image": "https://stat4.bollywoodhungama.in/wp-content/uploads/2024/04/Katrina-Kaif-turned-down-Hollywood-offer-recently-I-do-believe-it-will-happen-and-I-think-that-will-be-a-whole-new-leaf-in-my-book.jpg",
+        "name": "Katrina Kaif",
         "subject": "Physics",
         "experience": 9
     },
     {
-        "image": "https://picsum.photos/214",
-        "name": "Kevin Clark",
+        "image": "https://images.news18.com/ibnlive/uploads/2023/07/varun-dhawan-168904874516x9.png",
+        "name": "Varun Dhawan",
         "subject": "Economics",
         "experience": 7
     },
     {
-        "image": "https://picsum.photos/215",
-        "name": "Rebecca Lewis",
+        "image": "https://npg.news/h-upload/2024/04/04/1223610-kangana-ranaut.webp",
+        "name": "Kangana Ranaut",
         "subject": "Sociology",
         "experience": 5
     },
     {
-        "image": "https://picsum.photos/216",
-        "name": "Jason Walker",
+        "image": "https://www.radiocity.in/images/uploads/saifbday23main_d.jpg",
+        "name": "Saif Ali Khan",
         "subject": "Psychology",
         "experience": 11
     },
     {
-        "image": "https://picsum.photos/217",
-        "name": "Stephanie Hall",
+        "image": "https://www.hindustantimes.com/ht-img/img/2024/04/13/1600x900/vidya_balan_1712980590025_1712980590203.jpg",
+        "name": "Vidya Balan",
         "subject": "Political Science",
         "experience": 4
     },
     {
-        "image": "https://picsum.photos/218",
-        "name": "Edward Young",
+        "image": "https://akm-img-a-in.tosshub.com/lingo/cosmo/images/2023-03/shraddha_kapoor_interview_feature_2.png",
+        "name": "Shraddha Kapoor",
         "subject": "Philosophy",
         "experience": 10
     },
     {
-        "image": "https://picsum.photos/219",
-        "name": "Nicole King",
+        "image": "https://st1.bollywoodlife.com/wp-content/uploads/2023/05/rajinikanth.jpg",
+        "name": "Rajinikanth",
         "subject": "Religious Studies",
         "experience": 8
     },
     {
-        "image": "https://picsum.photos/220",
-        "name": "William Scott",
+        "image": "https://wp.missmalini.com/wp-content/uploads/2020/04/Sonam-Kapoor-Ahuja-4.jpg",
+        "name": "Sonam Kapoor Ahuja",
         "subject": "Environmental Science",
         "experience": 12
     },
     {
-        "image": "https://picsum.photos/221",
-        "name": "Michelle Adams",
+        "image": "https://www.healthkart.com/connect/wp-content/uploads/2020/09/john%20abraham.jpg",
+        "name": "John Abraham",
         "subject": "Health Education",
         "experience": 6
     },
     {
-        "image": "https://picsum.photos/222",
-        "name": "Thomas Green",
+        "image": "https://images.indianexpress.com/2024/03/ananya-panday-2.jpg",
+        "name": "Ananya Panday",
         "subject": "Drama",
         "experience": 9
     },
     {
-        "image": "https://picsum.photos/223",
-        "name": "Olivia Baker",
+        "image": "https://static.abplive.com/wp-content/uploads/2019/01/29171912/Tiger-Shroff-site.jpg",
+        "name": "Tiger Shroff",
         "subject": "Culinary Arts",
         "experience": 7
     },
     {
-        "image": "https://picsum.photos/224",
-        "name": "Richard Hill",
+        "image": "https://www.thestatesman.com/wp-content/uploads/2024/02/Kriti-Sanon.jpg",
+        "name": "Kriti Sanon",
         "subject": "Fashion Design",
         "experience": 5
     },
     {
-        "image": "https://picsum.photos/225",
-        "name": "Samantha Turner",
+        "image": "https://images.moneycontrol.com/static-mcnews/2024/05/20240511091916_sn.png",
+        "name": "Nawazuddin Siddiqui",
         "subject": "Woodworking",
         "experience": 11
     },
     {
-        "image": "https://picsum.photos/226",
-        "name": "George Cook",
+        "image": "https://images.indianexpress.com/2023/07/taapsee-pannu-200_insta.jpg",
+        "name": "Taapsee Pannu",
         "subject": "Metalworking",
         "experience": 4
     },
     {
-        "image": "https://picsum.photos/227",
-        "name": "Catherine Murphy",
+        "image": "https://media.cnn.com/api/v1/images/stellar/prod/200614084611-sushant-singh-rajput-restricted.jpg?q=x_3,y_1016,h_1684,w_2993,c_crop/h_833,w_1480",
+        "name": "Sushant Singh Rajput",
         "subject": "Auto Mechanics",
         "experience": 10
     },
     {
-        "image": "https://picsum.photos/228",
-        "name": "Peter Kelly",
+        "image": "https://static.toiimg.com/thumb/msid-89218532,width-400,resizemode-4/89218532.jpg",
+        "name": "Kiara Advani",
         "subject": "Electronics",
         "experience": 8
     },
     {
-        "image": "https://picsum.photos/229",
-        "name": "Jennifer Reed",
+        "image": "https://media.assettype.com/newindianexpress%2F2024-01%2Fb4c070f9-9d4e-4f26-8da1-532969c70994%2Fanimal_ranbir_kapoor.jpg",
+        "name": "Ranbir Kapoor",
         "subject": "Graphic Design",
         "experience": 12
     },
     {
-        "image": "https://picsum.photos/230",
-        "name": "Eric Morgan",
+        "image": "https://images.indianexpress.com/2022/07/sonakshi-sinha.jpg",
+        "name": "Sonakshi Sinha",
         "subject": "Web Development",
         "experience": 6
     },
     {
-        "image": "https://picsum.photos/231",
-        "name": "Angela Bell",
+        "image": "https://img.indiaforums.com/person/640x480/0/9946-ayushmann-khurrana.jpg?c=5nF5B1",
+        "name": "Ayushmann Khurrana",
         "subject": "Photography",
         "experience": 9
     },
     {
-        "image": "https://picsum.photos/232",
-        "name": "Paul Foster",
+        "image": "https://img.indiaforums.com/person/640x480/1/2908-disha-patani.jpg",
+        "name": "Disha Patani",
         "subject": "Physical Therapy",
         "experience": 7
     },
     {
-        "image": "https://picsum.photos/233",
-        "name": "Kelly Simmons",
+        "image": "https://static.toiimg.com/thumb/msid-107578752,width-1280,height-720,resizemode-4/107578752.jpg",
+        "name": "Shahid Kapoor",
         "subject": "Nursing",
         "experience": 5
     },
     {
-        "image": "https://picsum.photos/234",
-        "name": "Benjamin Cooper",
+        "image": "https://images.news18.com/ibnlive/uploads/2022/07/vicky-kaushal-anees-bazmee-16582467404x3.jpg",
+        "name": "Vicky Kaushal",
         "subject": "Veterinary Science",
         "experience": 11
     },
     {
-        "image": "https://picsum.photos/235",
-        "name": "Tiffany Rivera",
+        "image": "https://m.media-amazon.com/images/M/MV5BMTkzOTgzNzYzMl5BMl5BanBnXkFtZTgwMTI0MzY3OTE@._V1_.jpg",
+        "name": "Parineeti Chopra",
         "subject": "Astronomy",
         "experience": 4
     },
     {
-        "image": "https://picsum.photos/236",
-        "name": "Ronald Ward",
+        "image": "https://images.livemint.com/img/2020/11/13/1140x641/CHHALAANG_STILL_8_1605248640480_1605248658775.jpg",
+        "name": "Rajkummar Rao",
         "subject": "Engineering",
         "experience": 10
     },
     {
-        "image": "https://picsum.photos/237",
-        "name": "Amy Cox",
+        "image": "https://media.assettype.com/outlookindia/2024-02/670c62d4-3c0b-4870-8bf2-640f90013b98/Nora_Fatehi_featured_1.webp?w=1200&h=675&auto=format%2Ccompress&fit=max&enlarge=true",
+        "name": "Nora Fatehi",
         "subject": "Architecture",
         "experience": 8
     },
     {
-        "name": "Steven Powell",
-        "image": "https://picsum.photos/238",
+        "name": "Sanjay Dutt",
+        "image": "https://cdn.zeebiz.com/sites/default/files/2023/07/29/253788-image-1200x900-27.jpg",
         "subject": "Journalism",
         "experience": 12
     },
     {
-        "name": "Christine Long",
-        "image": "https://picsum.photos/239",
+        "name": "Janhvi Kapoor",
+        "image": "https://img.theweek.in/content/dam/week/magazine/theweek/leisure/images/2024/2/22/63-Janhvi-Kapoor.jpg",
         "subject": "Marketing",
         "experience": 6
     },
     {
-        "name": "Gregory Hughes",
-        "image": "https://picsum.photos/240",
+        "name": "Ishaan Khatter",
+        "image": "https://www.hindustantimes.com/ht-img/img/2023/07/09/1600x900/At-the-moment--actor-Ishaan-Khatter-is-busy-workin_1688882905578.jpg",
         "subject": "Business",
         "experience": 9
     },
     {
-        "name": "Pamela Butler",
-        "image": "https://picsum.photos/241",
+        "name": "Arjun Kapoor",
+        "image": "https://www.bollywoodhungama.com/wp-content/uploads/2022/03/Arjun-Kapoor-set-to-head-North-to-start-shooting-The-Lady-Killer.jpg",
         "subject": "Finance",
         "experience": 7
     },
     {
-        "name": "Keith Simmons",
-        "image": "https://picsum.photos/242",
+        "name": "Sara Ali Khan",
+        "image": "https://i0.wp.com/www.rvcj.com/wp-content/uploads/2024/04/Sara-Ali-Khan-Says-No-One-Dares-To-Use-Pick-Up-Lines-On-Her.jpg",
         "subject": "Entrepreneurship",
         "experience": 5
     },
     
     {
-        "name": "John Smith",
-        "image": "https://picsum.photos/243",
+        "name": "Rakul Preet Singh",
+        "image": "https://filmfare.wwmindia.com/content/2022/sep/rakul-preet-singh-21664430221.jpg",
         "subject": "Math",
         "experience": 10
     },
     {
-        "image": "https://picsum.photos/244",
-        "name": "Jane Doe",
+        "image": "https://stat4.bollywoodhungama.in/wp-content/uploads/2017/02/Farhan-Akhtar.jpg",
+        "name": "Farhan Akhtar",
         "subject": "English",
         "experience": 8
     },
     {
-        "image": "https://picsum.photos/245",
-        "name": "Michael Johnson",
+        "image": "https://www.informalnewz.com/wp-content/uploads/2023/03/Anil-kapoor.jpg",
+        "name": "Anil Kapoor",
         "subject": "Science",
         "experience": 12
     },
     {
-        "name": "Emily Davis",
-        "image": "https://picsum.photos/246",
+        "name": "Tabu",
+        "image": "https://i.ndtvimg.com/mt/movies/2012-11/tabu-enter-read.jpg",
         "subject": "History",
         "experience": 6
     },
     {
-        "name": "Robert Wilson",
+        "name": "Madhuri Dixit",
         "subject": "Art",
-        "image": "https://picsum.photos/247",
+        "image": "https://assets.telegraphindia.com/telegraph/2024/Apr/1713609324_madhuri-dixit.jpg",
         "experience": 15
     },
     {
-        "name": "Sarah Thompson",
+        "name": "Kajol",
         "subject": "Music",
-        "image": "https://picsum.photos/248",
+        "image": "https://i.ndtvimg.com/mt/movies/2013-08/kajol-ddlj.jpg",
         "experience": 9
     },
     {
-        "name": "David Brown",
-        "image": "https://picsum.photos/249",
+        "name": "Jethalal Gada",
+        "image": "https://www.koimoi.com/wp-content/new-galleries/2020/07/taarak-mehta-ka-ooltah-chashmah-did-you-know-jethalals-character-possess-this-shop-not-gada-electronics-in-original-literature-0001.jpg",
         "subject": "Physical Education",
         "experience": 11
     },
     {
-        "image": "https://picsum.photos/250",
-        "name": "Jennifer Taylor",
+        "image": "https://origin-staticv2.sonyliv.com/landscape_thumb/tmkoc_landscape_thumbnail_ep06.jpg",
+        "name": "Champak lal Gada",
         "subject": "Geography",
         "experience": 7
     },
     {
-        "name": "Daniel Anderson",
-        "image": "https://picsum.photos/251",
+        "name": "Daya Gada",
+        "image": "https://images.indianexpress.com/2022/05/disha-vakani-taarak-1200.jpg",
         "subject": "Computer Science",
         "experience": 14
     },
     {
-        "name": "Laura Martinez",
-        "image": "https://picsum.photos/252",
+        "name": "Tapu Gada",
+        "image": "https://starsunfolded.com/wp-content/uploads/2017/02/Bhavya-Gandhi.jpg",
         "subject": "Foreign Language",
         "experience": 5
     },
     {
-        "image": "https://picsum.photos/253",
-        "name": "Christopher Thomas",
+        "image": "https://feeds.abplive.com/onecms/images/uploaded-images/2022/02/06/41f4d59d0933fd46d513a25715afdb34_original.jpg?impolicy=abp_cdn&imwidth=720",
+        "name": "Atmaram Bhide",
         "subject": "Social Studies",
         "experience": 13
     },
     {
-        "image": "https://picsum.photos/254",
-        "name": "Amanda Hernandez",
+        "image": "https://www.koimoi.com/wp-content/new-galleries/2020/04/sonalika-joshi-001.jpg",
+        "name": "Madhvi Bhide",
         "subject": "Drama",
         "experience": 7
     },
     {
-        "image": "https://picsum.photos/255",
-        "name": "Matthew Robinson",
+        "image": "https://d30ny7ijak9wq4.cloudfront.net/s3fs-public/images/story/2013/08/03/Nidhi%20Bhanushali.jpg",
+        "name": "Sonu Bhide",
         "subject": "Biology",
         "experience": 10
     },
     {
-        "image": "https://picsum.photos/256",
-        "name": "Stephanie Lewis",
+        "image": "https://images.indianexpress.com/2021/10/Gurucharan-Singh-roshan-sodhi-TMKOC-1.jpg",
+        "name": "Roshan Singh Sodhi",
         "subject": "Chemistry",
         "experience": 8
     },
     {
-        "image": "https://picsum.photos/257",
-        "name": "Joseph Walker",
+        "image": "https://akm-img-a-in.tosshub.com/aajtak/images/story/202209/shyam-pathak_popatlal_fb-7-sixteen_nine.jpg?size=1200:675",
+        "name": "Popatlal",
         "subject": "Physics",
         "experience": 12
     },
     {
-        "name": "Rebecca Hall",
-        "image": "https://picsum.photos/258",
+        "name": "Goli Hathi",
+        "image": "https://images.tv9hindi.com/wp-content/uploads/2021/04/kush-shah.jpg",
         "subject": "Economics",
         "experience": 6
     },
     {
-        "name": "Samuel Young",
-        "image": "https://picsum.photos/259",
+        "name": "Tarak Maheta",
+        "image": "https://i.ytimg.com/vi/LbYSsX7t-V4/maxresdefault.jpg",
         "subject": "Political Science",
         "experience": 9
     },
     {
-        "name": "Melissa White",
-        "image": "https://picsum.photos/260",
+        "name": "Anjali Maheta",
+        "image": "https://www.bollywoodhungama.com/wp-content/uploads/2020/09/Neha-Mehta-wanted-to-return-to-Taarak-Mehta-Ka-Ooltah-Chashmah-had-even-spoken-to-Asit-Modi.jpeg",
         "subject": "Sociology",
         "experience": 11
     },
     {
-        "name": "Samuel Young",
+        "name": "Krishnan Iyer ",
         "subject": "Political Science",
-        "image": "https://picsum.photos/261",
+        "image": "https://images.tv9marathi.com/wp-content/uploads/2023/01/22192618/Krishnan-Iyer-tv9.jpg",
         "experience": 9
     },
     {
-        "name": "Melissa White",
-        "image": "https://picsum.photos/262",
+        "name": "Babitaji Gada",
+        "image": "https://i.pinimg.com/736x/f8/e8/e7/f8e8e7628f576cb188490666d38da103.jpg",
         "subject": "Sociology",
         "experience": 11
     }
-
 ]
 
 export default teacherdata;
